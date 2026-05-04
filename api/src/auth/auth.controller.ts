378b5625766226eb22d4fd6f27f9954b8ecc8f7b
julyperfect07 <abdallah.july05@gmail.com>
2026-05-04 13:35:32 +0300

    send the token in a cookie

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
+import type { Response } from 'express';
 import { LocalGuard } from './guards/local.guard';
 
 @Controller('auth')
@@ -8,12 +9,25 @@ export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('login')
   @UseGuards(LocalGuard)
-  login(@Body() authDto: AuthDto) {
-    return this.authService.validateUser(authDto);
+  async login(@Body() authDto: AuthDto, @Res() res: Response) {
+    const result = await this.authService.validateUser(authDto);
+    res.cookie('access_token', result.access_token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+      maxAge: 60 * 60 * 1000, // 1 hour
+    });
+    return res.json({ message: 'Login successful' });
   }
 
   @Post('register')
   register(@Body() authDto: AuthDto) {
     return this.authService.register(authDto);
+  }
+
+  @Post('logout')
+  logout(@Res() res: Response) {
+    res.clearCookie('access_token');
+    return res.json({ message: 'Logout successful' });
   }
 }
--- api/src/main.ts
+++ api/src/main.ts
@@ -2,10 +2,12 @@ import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   console.log('DB URL loaded:', !!process.env.DATABASE_URL); // should now be true
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
