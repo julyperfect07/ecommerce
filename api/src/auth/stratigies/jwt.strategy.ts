b73c0e6dbceb1816791939762fc67bce76839674
julyperfect07 <abdallah.july05@gmail.com>
2026-05-09 12:12:54 +0300

    Fixed jwt secret

--- api/src/auth/stratigies/jwt.strategy.ts
+++ api/src/auth/stratigies/jwt.strategy.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
