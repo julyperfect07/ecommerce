0d7dcc9cd6fec45e7227bdee9bd235bf01479be8
julyperfect07 <abdallah.july05@gmail.com>
2026-05-10 14:02:51 +0300

    create cart module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ProductsModule } from './products/products.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule, ProductsModule],
+  imports: [PrismaModule, AuthModule, UserModule, ProductsModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api/src/cart/cart.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { CartController } from './cart.controller';
+import { CartService } from './cart.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [PrismaModule, AuthModule],
+  controllers: [CartController],
+  providers: [CartService],
+})
+export class CartModule {}
