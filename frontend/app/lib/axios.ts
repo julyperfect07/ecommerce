b6e965fd8befff01d2fb6f2e654b42f49b94b580
julyperfect07 <abdallah.july05@gmail.com>
2026-05-21 13:34:39 +0300

    add comments to axios file

--- frontend/app/lib/axios.ts
+++ frontend/app/lib/axios.ts
@@ -4,31 +4,46 @@ import axios, {
   InternalAxiosRequestConfig,
 } from "axios";
 
+// Create custom axios instance
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-  withCredentials: true,
+  baseURL: "http://localhost:3000", // Backend URL
+  withCredentials: true, // Send cookies automatically
 });
 
+// Response interceptor
 api.interceptors.response.use(
+  // If request succeeds
   (response: AxiosResponse) => {
     return response;
   },
+
+  // If request fails
   async (error: AxiosError) => {
+    // Get failed request
     const originalRequest = error.config as InternalAxiosRequestConfig & {
       _retry: boolean;
     };
 
+    // If token expired and request not retried yet
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
+
       try {
+        // Try refreshing token
         await api.post("/auth/refresh");
+
+        // Retry original request
         return api(originalRequest);
       } catch {
+        // If refresh fails redirect to login
         window.location.href = "/login";
       }
     }
+
+    // Return error normally
     return Promise.reject(error);
   },
 );
 
+// Export axios instance
 export default api;
